17b21ffb9cea77c336ce946a32ac482c41292205
Bodunde Adebiyi <bodunadebiyi@gmail.com>
2018-10-22 21:09:06 +0100

    add static_root to settings

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR)
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "static")
 ]
 
 LOGIN_REDIRECT_URL = 'goto_dashboard'
